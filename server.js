3f2870859b04b0964e782a986b578ff1d8fd2c28
hardikajmani <hardikajmani@gmail.com>
2017-02-03 12:12:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,17 @@ app.get('/', function (req, res) {
 
 app.get('/art-one',function (req,res){
     res.send("Article-one will be served here");
-})
+});
+
+app.get('/art-two',function (req,res){
+    res.send("Article-two will be served here");
+});
+
+app.get('/art-three',function (req,res){
+    res.send("Article-three will be served here");
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
