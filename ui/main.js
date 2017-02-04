6ae879c84a01b445487186e6ec1cefa3677b920e
hardikajmani <hardikajmani@gmail.com>
2017-02-04 16:55:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ var interval = setInterval(moveRight, 100);
 
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 
