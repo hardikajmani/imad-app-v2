94be4a248054b8b4a3969d348eb9e0681e5a84cb
hardikajmani <hardikajmani@gmail.com>
2017-02-04 16:34:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 
 alert('Hy I\' m alert box and the js file is loaded'  );
+
+//Id is unique but classes can be multiple and can be common too!!
+
+var element = document.getElementById('info');
+element.innerHTML = "I changed some text !!!";
