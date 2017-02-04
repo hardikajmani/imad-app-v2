77c6fadce5f2f3ce869654f6252f7a8c5f1ff2c7
hardikajmani <hardikajmani@gmail.com>
2017-02-04 16:40:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,15 @@ console.log('Loaded!');
 
 alert('Hy I\' m alert box and the js file is loaded'  );
 
-
+/*
 var element = document.getElementById('info');
-element.innerHTML = "I changed some text !!!";
+element.innerHTML = "I changed some text !!!";*/
+
+//Use above code to change something by getting that element by its id name
+
 //Id is unique but classes can be multiple and can be common too!!//
+
+var img = document.getElementById('me');
+img.onclick = function () {
+    img.style.marginleft ='100px';
+};
