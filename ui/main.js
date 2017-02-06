9662e873a90116980c8304fab6a61c66fb6ad7fd
hardikajmani <hardikajmani@gmail.com>
2017-02-06 18:52:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('loaded');
+//console.log('loaded');
 
 /*var button = document.getElementById('counter');
 
@@ -28,7 +28,7 @@ button.onclick = function (){
 };*/
 
 var cmmtButton = document.getElementById('cmmtSubmit');
-console.log(document.title);
+//console.log(document.title);
 cmmtButton.onclick = function (){
     
     // Create request
