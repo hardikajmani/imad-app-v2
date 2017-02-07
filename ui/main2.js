b998ba6b8db6c8680172b3b93cd293d5c5b34288
hardikajmani <hardikajmani@gmail.com>
2017-02-07 14:36:02 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -41,7 +41,97 @@
          
         var nameVal = document.getElementById(cmmtName).value;
         
-        cmmtRequest.open('GET','http://hardikajmani.imad.hasura-app.io/submitCmmtName/' + num)
+        cmmtRequest.open('GET','http://hardikajmani.imad.hasura-app.io/submitCmmtName/' + num + '?name=' + nameVal,true);
+        cmmt.Request.send(null);
+    
+    };
+    
+   
+    cmmtButton.onclick = function (){
+        
+        // Create request
+            var cmmtRequest = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+            cmmtRequest.onreadystatechange = function(){
+                
+                if(cmmtRequest.readyState === XMLHttpRequest.DONE)
+                
+                    if( cmmtRequest.status === 200){
+                        var counter = cmmtRequest.responseText;
+                        var span = document.getElementById('cmmtCount');
+                        span.innerHTML = counter.toString();
+                    }
+            }; 
+         //choose the correct url for the request
+         var name = document.title;
+         var num = 'one';
+         console.log(name);
+         switch(name){
+             
+            case 'Myself' : num = 'one';
+                            break;
+                            
+            case 'Hobbies' : num = 'two';
+                             break;
+                             
+            case 'AIM': num = '3';
+                        break;
+         }
+        
+        
+        
+        //Make the request
+        
+         
+        var cmmtVal = document.getElementById(cmmt).value;
+        
+        cmmtRequest.open('GET','http://hardikajmani.imad.hasura-app.io/submitCmmtName/' + num + '?name=' + cmmtVal,true);
+        cmmt.Request.send(null);
+    
+    };
+   
+    cmmtButton.onclick = function (){
+        
+        // Create request
+            var cmmtRequest = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+            cmmtRequest.onreadystatechange = function(){
+                
+                if(cmmtRequest.readyState === XMLHttpRequest.DONE)
+                
+                    if( cmmtRequest.status === 200){
+                        var counter = cmmtRequest.responseText;
+                        var span = document.getElementById('cmmtCount');
+                        span.innerHTML = counter.toString();
+                    }
+            }; 
+         //choose the correct url for the request
+         var name = document.title;
+         var num = 'one';
+         console.log(name);
+         switch(name){
+             
+            case 'Myself' : num = 'one';
+                            break;
+                            
+            case 'Hobbies' : num = 'two';
+                             break;
+                             
+            case 'AIM': num = '3';
+                        break;
+         }
+        
+        
+        
+        //Make the request
+        
+         
+        var nameVal = document.getElementById(cmmtName).value;
+        
+        cmmtRequest.open('GET','http://hardikajmani.imad.hasura-app.io/submitCmmtName/' + num + '?name=' + nameVal,true);
+        cmmt.Request.send(null);
     
     };
     
