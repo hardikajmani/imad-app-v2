8816c61df554c6f59d6ec56a9ecc01b2a3016809
hardikajmani <hardikajmani@gmail.com>
2017-03-09 17:26:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
+
+
+var config = {
+    user : 'hardikajmani',
+    database : 'hardikajmani',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+    
+};
+
+
 
 var articles = {
    'art-one' : {
@@ -103,6 +115,19 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    //make a request 
+    ppol.query('SELECT * from test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else{
+            res.send(JSON.stringify(result));
+        }
+    })
+    //return the response with the results
+})
 
 var counter = 0;
 app.get('/cs',function (req, res){
