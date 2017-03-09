837adafec42a4cfe1421de1dd1f23e35f1677dae
hardikajmani <hardikajmani@gmail.com>
2017-03-09 17:28:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //make a request 
-    ppol.query('SELECT * from test',function(err,result){
+    pool.query('SELECT * from test',function(err,result){
         if(err){
             res.status(500).send(err.toString());
         } else{
