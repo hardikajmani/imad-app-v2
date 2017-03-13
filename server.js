9761969cb9ce05f4147c41a30f589f11e502240a
hardikajmani <hardikajmani@gmail.com>
2017-03-13 19:02:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,42 +17,7 @@ var config = {
 
 
 
-var articles = {
-   'art-one' : {
-                title : "Myself",
-                heading : "About Myself",
-                content : `<p>
-                                    In this article I'm going to tell a little bit about myself.
-                                <br/> My name is Hardik Ajmani and I was born in Ambala,Haryana,
-                                <br/> Now my famliy live in Mumbai and I am studying in Chennai in SRM.
-                                <br/> This is my first article.
-                                <br/> I love programming!!!
-                                </p>`
-    
-},
-   'art-two' : {
-                title : "Hobbies",
-                heading : "My hobbies",
-                content : `<p> I have many hobbies ans some of them are:</p>
-                                <ul>
-                                    <li>Programming</li>
-                                    <li>Making new things</li>
-                                    <li>Gaming</li>
-                                    <li>Photography</li>
-                                </ul>`
-    
-},
 
-  'art-three' : {
-                title : "AIM",
-                heading : "AIM",
-                content : `<p>Hey everyone in this article I'm going to tell about my career.
-                                <br/> I am planning to pursue MS in Artificial Intelligence from MIT after completing my B.tech which I am doing in   SRM University, Chennai. 
-                                <br/> Then I may or may not pursue a PHd but I surely want to work with NASA at some point of my life.
-                                </p>`
-    
-}
-};
 
 function createTemplate(data){
 
