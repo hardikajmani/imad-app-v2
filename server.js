d19f1a64d5400b62c4711d00fd9b3816030704ee
hardikajmani <hardikajmani@gmail.com>
2017-03-13 18:56:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.get('/submitCmmtName/:siteName',function(req, res){ // query format url sfff
 
 app.get('/article/:artName',function (req,res){
   
-  pool.query("SELECT * FROM articles WHERE title = '" + req.params.artName + "'", function(err,result){
+  pool.query("SELECT * FROM articles WHERE title = $1",[req.params.artName], function(err,result){
       if(err){
           res.status(500).send(err.Tostring());
       } else {
